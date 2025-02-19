72f664d5cd66bd04bf400e221823ce8841bb0951
Vishal Hacker <vk0549433@gmail.com>
2025-02-19 15:17:28 +0530

    Update launch.sh

--- .package/launch.sh
+++ .package/launch.sh
@@ -1,1 +1,13 @@
 
+#!/bin/bash
+
+DISTRO=$(uname -o)
+
+if [ $DISTRO == Android ]; then
+	export XPHISHER_ROOT="/data/data/com.termux/files/usr/opt/Zxphisher"
+else
+	export XPHISHER_ROOT="/usr/opt/Zxphisher"
+fi
+
+cd $ZxPHISHER_ROOT
+bash ./Zxphisher.sh
